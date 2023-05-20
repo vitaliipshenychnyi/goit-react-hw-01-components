a69cf383e911229dfe960ea62a3acaa6523c6fcb
VitaliiPshenychniy <4kvaap@gmail.com>
2023-05-20 19:57:00 +0300

    add TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import { FriendList } from './friendlist/FriendList';
+import { TransactionHistory } from './transactionhistory/TransactionHistory';
 import user from './profile/user.json';
 import data from './statistics/data.json';
-import friends from './friendlist/friends.json'
+import friends from './friendlist/friends.json';
+import transactions from './transactionhistory/transactions.json';
 
 export const App = () => {
   return (
     <div
       style={{
-        // height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         gap: 100,
@@ -17,6 +18,7 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        margin: 50,
       }}
     >
       <Profile
@@ -32,6 +34,8 @@ export const App = () => {
       <Statistics title={data.title} stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
